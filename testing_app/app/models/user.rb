31afe88f53825581b052a42d20772151aa0255f9
Sreyashi <sreyashi@gmail.com>
2018-08-23 22:35:46 +0000

    rspec introduction

---
+++ testing_app/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+    
+    scope:active_users, -> {where(active: true)}
+    scope:inactive_users, -> {where(active: false)}
+    validates :first_name, presence:true
+    validates :last_name, presence:true
+    validates :email, presence:true
+    
+end
